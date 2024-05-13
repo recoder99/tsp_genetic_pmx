d0059d05e2885985a17d067d3be7069f2b65939c
recoder99 <30973326+recoder99@users.noreply.github.com>
2024-05-13 21:17:05 +0800

    second_commit

--- tsp_pmx.py
+++ tsp_pmx.py
@@ -1,3 +1,5 @@
+import math
+
 # mp = {{0, 10, 15, 20},
 #      {10, 0, 35, 25},
 #      {15, 35, 0, 30},
@@ -8,28 +10,41 @@ def swap(arr1, arr2, start, end):
     for i in range(start, end + 1):
         arr1[i], arr2[i] = arr2[i], arr1[i]
 
-def mutate(p1, p2):
+def crossover(p1, p2):
     c1 = p1
     c2 = p2
 
-    swap(c1, c2, 1,3)
+    swap(c1, c2, 1,2)
+
+    index1 = []
+    index2 = []
+
+    for i in range(len(c1)):
+        for j in range(len(c1)):
+            if (c1[j] == c1[i]) and (i != j) and ():
+                index1.append(j)
+
+    for i in range(len(c2)):
+        for j in range(len(c2)):
+            if (c2[j] == c2[i]) and (i != j):
+                index2.append(j)
+
+    index2 = index2.reverse()
+    #crossing
+    for i in range(len(index1)):
+        c1[index1[i]], c2[index1[i]] = c2[index1[i]], c1[index1[i]]
+
 
     print(c1)
     print(c2)
 
-    # for i in range(len(c1)):
-    #     for j in range(len(c2)):
-    #         c1[]
-
-
  
- 
-array1 = [1, 2, 3, 4, 5]
-array2 = [6, 7, 8, 9, 10]
+array1 = [2, 3, 5, 1]
+array2 = [5, 4 ,2, 1]
  
 # start = int(input("Enter the starting index: "))
 # end = int(input("Enter the ending index: "))
  
-mutate(array1, array2)
+crossover(array1, array2)
  
 
