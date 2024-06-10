ca25ffd14cdfd54315a026d35a3d171a45ce03ca
binaryjep <jeffescueta13@gmail.com>
2024-06-10 18:56:12 +0800

    speed fuzzy logic

--- temp_cover.py
+++ temp_cover.py
@@ -1,168 +1,118 @@
-import numpy as np
-import math
-import matplotlib.pyplot as plt
+##########################################################################
 
-###########################################################################
-
-def temp(temp: int):
-    f = temp_freezing(temp)
-    c = temp_cool(temp)
-    w = temp_warm(temp)
-    h = temp_hot(temp)
-
-    return f, c, w, h
-
-def temp_freezing(temp: int):
-    if 0 <= temp <= 30:
+def calcFreezing(temp):
+    if 0 >= temp < 30:
         return 1
-    elif 30 < temp <= 50:
+    elif 30 <= temp < 50:
         return -0.05 * temp + 2.5
-    elif 50 < temp <= 110:
+    elif 50 <= temp < 70:
+        return 0
+    elif 70 <= temp < 90:
         return 0
     else:
-        return -1
-
-def temp_cool(temp: int):
-    if 0 <= temp <= 30:
         return 0
-    elif 30 < temp <= 50:
-        return 0.05 * temp - 1.5
-    elif 50 < temp <= 70:
-        return -0.05 * temp + 3.5
-    elif 70 < temp <= 110:
-        return 0
-    else:
-        return -1
-
-def temp_warm(temp: int):
-    if 0 <= temp <= 50:
-        return 0
-    elif 50 < temp <= 70:
-        return 0.05 * temp - 2.5
-    elif 70 < temp <= 90:
-        return -0.05 * temp + 4.5
-    elif 90 < temp <= 110:
-        return 0
-    else:
-        return -1
-
-def temp_hot(temp: int):
-    if 0 <= temp <= 70:
-        return 0
-    elif 70 < temp <= 90:
-        return 0.05 * temp - 3.5
-    elif 90 < temp <= 110:
-        return 1
-    else:
-        return -1
-
-###########################################################################
-
-def cover(cloud_cover: int):
-    s = cover_sunny(cloud_cover)
-    p = cover_partlycloudy(cloud_cover)
-    o = cover_overcast(cloud_cover)
-
-    return s, p, o
-
-def cover_sunny(cloud: int):
-    if 0 <= cloud <= 20:
-        return 1
-    elif 20 < cloud <= 40:
-        return -0.05 * cloud + 2
-    elif 40 < cloud <= 100:
-        return 0
-    else:
-        return -1
-
-def cover_partlycloudy(cloud: int):
-    if 0 <= cloud <= 20:
-        return 0
-    elif 20 < cloud <= 50:
-        return 0.03 * cloud - 0.67
-    elif 50 < cloud <= 80:
-        return -0.03 * cloud + 2.67
-    elif 80 < cloud <= 100:
-        return 0
-    else:
-        return -1
-
-def cover_overcast(cloud: int):
-    if 0 <= cloud <= 60:
-        return 0
-    if 60 < cloud <= 80:
-        return 0.05 * cloud - 3
-    elif 80 <= cloud:
-        return 1
-    else:
-        return -1
-
-###########################################################################
     
-def getSpeed(fast, slow):
-    f = fast
-    s = slow
+def calcCool(temp):
+    if 0 >= temp < 30:
+        return 0
+    elif 30 <= temp < 50:
+        return 0.05 * temp - 1.5
+    elif 50 <= temp < 70:
+        return -0.05 * temp + 3.5
+    elif 70 <= temp < 90:
+        return 0
+    else:
+        return 0
+    
+def calcWarm(temp):
+    if 0 >= temp < 30:
+        return 0
+    elif 30 <= temp < 50:
+        return 0
+    elif 50 <= temp < 70:
+        return 0.05 * temp - 2.5
+    elif 70 <= temp < 90:
+        return -0.05 * temp + 4.5
+    else:
+        return 0
+    
+def calcHot(temp):
+    if 0 >= temp < 30:
+        return 0
+    elif 30 <= temp < 50:
+        return 0
+    elif 50 <= temp < 70:
+        return 0
+    elif 70 <= temp < 90:
+        return 0.05 * temp - 3.5
+    else:
+        return 1
+    
+def determineTemp(temp):
+    freezing = calcFreezing(temp)
+    cool = calcCool(temp)
+    warm = calcWarm(temp)
+    hot = calcHot(temp)
 
-    fY = ((f - -0.5) / 0.02)
-    sY = ((s - -0.5) / 0.02)
+    return freezing, cool, warm, hot
 
-    fY = math.ceil(fY)
-    sY = math.ceil(sY)
+##########################################################################
 
-    y = np.zeros(101)
+def calcSunny(clouds):
+    if 0 <= clouds < 20:
+        return 1
+    elif 20 <= clouds < 40:
+        return -0.05 * clouds + 2
+    else:
+        return 0
+    
+def calcPartlyCloudy(clouds):
+    if 0 <= clouds < 20:
+        return 0
+    elif 20 <= clouds < 40:
+        return 0.033333333333333 * clouds - 0.67
+    elif 40 <= clouds < 80:
+        return -0.033333333333333 * clouds + 2.67
+    else:
+        return 0
+    
+def calcOvercast(clouds):
+    if 0 <= clouds < 60:
+        return 0
+    elif 60 <= clouds < 80:
+        return 0.05 * clouds - 3
+    else:
+        return 1
+    
+def determineCloudCover(clouds):
+    sunny = calcSunny(clouds)
+    partly_cloudy = calcPartlyCloudy(clouds)
+    overcast = calcOvercast(clouds)
+    
+    return sunny, partly_cloudy, overcast
 
-    print(y)
+##########################################################################
 
-    y[:math.ceil(sY)] = s
-    y[math.ceil(fY):] = f
+def calculateSpeed(fast, slow):
+    if slow + fast == 0:
+        return 0
+    
+    speed = ((slow * 25) + (fast * 75)) / (slow + fast)
+    return speed
 
-    steps = fY - sY
-    delta_y = f - s
-    step_size = delta_y / steps
+##########################################################################
 
-    for i in range(sY, fY):
-        y[i] = s + (i - sY) * step_size
+temperature = int(input("Enter temperature: "))
+cover = int(input("Enter cover: "))
 
-    sumY = 0.00
-    sumXY = 0.00
+f, c, w, h = determineTemp(temperature)
+s, p, o = determineCloudCover(cover)
 
-    x_values = []
-    y_values = []
+fast = min(s, w)
+slow = min(p, c)
 
-    for i in range(101):
-        x_values.append(i)
-        y_values.append(y[i])
-        sumXY = sumXY + (i * y[i])
-        sumY = sumY + y[i]
+speed = calculateSpeed(fast, slow)
 
-    plt.plot(x_values, y_values)
-    plt.xlabel('X values')
-    plt.ylabel('Y values')
-    plt.title('Y values over X')
-    plt.show()
+print(f"Speed: {speed:.5f}")
 
-    return sumXY / sumY
-
-###########################################################################
-
-if __name__ == '__main__':
-    t = int(input())
-    c = int(input())
-
-    tR = temp(t)
-    cR = cover(c)
-
-    print(f'Temperature-------------------\nFreezing: {tR[0]} || Cool: {tR[1]} || Warm: {tR[2]} || Hot: {tR[3]}')
-    print(f'Cloud Cover-------------------\nSunny: {cR[0]} || Partly Cloudy: {cR[1]} || Overcast: {cR[2]}')
-
-    fast = min(tR[2], cR[0])
-    slow = min(tR[1], cR[1])
-
-    speed = getSpeed(fast, slow)
-
-    print("Output-------------------")
-    print("Fast: ", fast)
-    print("Slow: ", slow) 
-    print("Speed: ", speed)
-
-###########################################################################
+##########################################################################
